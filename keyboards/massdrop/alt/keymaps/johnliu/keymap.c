6ef212ec0b59356bf70205946c9a6aa650c66a93
John Liu <john.liu@discordapp.com>
2020-08-10 11:08:34 -0700

    new rgb lighting api

--- keyboards/massdrop/alt/keymaps/johnliu/keymap.c
+++ keyboards/massdrop/alt/keymaps/johnliu/keymap.c
@@ -1,16 +1,6 @@
 #include QMK_KEYBOARD_H
 
 enum keycodes {
-    L_BRI = SAFE_RANGE, //LED Brightness Increase
-    L_BRD,              //LED Brightness Decrease
-    L_PTN,              //LED Pattern Select Next
-    L_PTP,              //LED Pattern Select Previous
-    L_PSI,              //LED Pattern Speed Increase
-    L_PSD,              //LED Pattern Speed Decrease
-    L_T_MD,             //LED Toggle Mode
-    L_T_ONF,            //LED Toggle On / Off
-    L_T_BR,             //LED Toggle Breath Effect
-    L_T_PTD,            //LED Toggle Scrolling Pattern Direction
     PC1,
     PC2
 };
@@ -22,41 +12,11 @@ enum layers {
     OFF
 };
 
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 255
+#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_GRADIENT_LEFT_RIGHT
+#define RGB_MATRIX_STARTUP_VAL 192
+
 #define SEND_CMD(x) SEND_STRING(SS_TAP(X_LCTRL) SS_DELAY(50) SS_TAP(X_LCTRL) SS_DELAY(50) x)
-
-/*
-enum tap_states {
-    SINGLE_TAP,
-    SINGLE_HOLD,
-    DOUBLE_TAP,
-    DOUBLE_HOLD,
-    DOUBLE_SINGLE_TAP
-};
-
-enum tap_dances {
-    KVM,
-};
-
-uint8_t cur_dance(qk_tap_dance_state_t *state) {
-    if (state->count == 1) {
-        if (state->interrupted || !state->pressed) return SINGLE_TAP;
-        else return SINGLE_HOLD;
-    }
-    return 2;
-};
-
-void handle_kvm(qk_tap_dance_state_t *state, void *user_data) {
-    if (cur_dance(state) == SINGLE_HOLD) {
-        SEND_CMD("u");
-    } else {
-        tap_code(KC_PGDN);
-    }
-};
-
-qk_tap_dance_action_t tap_dance_actions[] = {
-    [KVM] = ACTION_TAP_DANCE_FN(handle_kvm),
-};
-*/
 
 keymap_config_t keymap_config;
 
@@ -79,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TO(NIL), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RESET,   \
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, NK_TOGG, \
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, \
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, L_T_ONF, \
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, RGB_TOG, \
         XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX,                            TO(OFF), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX  \
     ),
     [OFF] = LAYOUT(
@@ -113,8 +73,17 @@ bool is_pc2_held = false;
 uint16_t pc1_timer = 0;
 uint16_t pc2_timer = 0;
 
+void rgb_init(void) {
+    rgb_matrix_set_flags(LED_FLAG_ALL);
+    rgb_matrix_mode(RGB_MATRIX_STARTUP_MODE);
+    rgb_matrix_sethsv(HSV_GOLDENROD);
+    rgb_matrix_sethsv(rgb_matrix_get_hue(), rgb_matrix_get_sat(), RGB_MATRIX_STARTUP_VAL);
+    rgb_matrix_config.speed = 32;
+}
+
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
+    rgb_init();
 };
 
 // Runs constantly in the background, in a loop.
@@ -127,72 +96,37 @@ void matrix_scan_user(void) {
     }
 };
 
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+        case NIL:
+            rgb_init();
+            break;
+        case FN:
+            rgb_matrix_set_flags(LED_FLAG_ALL);
+            rgb_matrix_mode(RGB_MATRIX_STARTUP_MODE);
+            rgb_matrix_sethsv(rgb_matrix_get_hue(), rgb_matrix_get_sat(), RGB_MATRIX_MAXIMUM_BRIGHTNESS);
+            break;
+        case META:
+            rgb_matrix_set_flags(LED_FLAG_ALL);
+            rgb_matrix_mode(RGB_MATRIX_STARTUP_MODE);
+            rgb_matrix_sethsv(rgb_matrix_get_hue(), rgb_matrix_get_sat(), 128);
+            break;
+        case OFF:
+            rgb_matrix_config.speed = 64;
+            rgb_matrix_set_flags(LED_FLAG_KEYLIGHT);
+            rgb_matrix_set_color_all(0, 0, 0);
+
+            rgb_matrix_set_flags(LED_FLAG_UNDERGLOW);
+            rgb_matrix_sethsv(HSV_WHITE);
+
+            rgb_matrix_mode(RGB_MATRIX_BREATHING);
+            break;
+    }
+    return state;
+};
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case L_BRI:
-            if (record->event.pressed) {
-                if (LED_GCR_STEP > LED_GCR_MAX - gcr_desired) gcr_desired = LED_GCR_MAX;
-                else gcr_desired += LED_GCR_STEP;
-                if (led_animation_breathing) gcr_breathe = gcr_desired;
-            }
-            return false;
-        case L_BRD:
-            if (record->event.pressed) {
-                if (LED_GCR_STEP > gcr_desired) gcr_desired = 0;
-                else gcr_desired -= LED_GCR_STEP;
-                if (led_animation_breathing) gcr_breathe = gcr_desired;
-            }
-            return false;
-        case L_PTN:
-            if (record->event.pressed) {
-                if (led_animation_id == led_setups_count - 1) led_animation_id = 0;
-                else led_animation_id++;
-            }
-            return false;
-        case L_PTP:
-            if (record->event.pressed) {
-                if (led_animation_id == 0) led_animation_id = led_setups_count - 1;
-                else led_animation_id--;
-            }
-            return false;
-        case L_PSI:
-            if (record->event.pressed) {
-                led_animation_speed += ANIMATION_SPEED_STEP;
-            }
-            return false;
-        case L_PSD:
-            if (record->event.pressed) {
-                led_animation_speed -= ANIMATION_SPEED_STEP;
-                if (led_animation_speed < 0) led_animation_speed = 0;
-            }
-            return false;
-        case L_T_MD:
-            if (record->event.pressed) {
-                led_lighting_mode++;
-                if (led_lighting_mode > LED_MODE_MAX_INDEX) led_lighting_mode = LED_MODE_NORMAL;
-            }
-            return false;
-        case L_T_ONF:
-            if (record->event.pressed) {
-                led_enabled = !led_enabled;
-                I2C3733_Control_Set(led_enabled);
-            }
-            return false;
-        case L_T_BR:
-            if (record->event.pressed) {
-                led_animation_breathing = !led_animation_breathing;
-                if (led_animation_breathing) {
-                    gcr_breathe = gcr_desired;
-                    led_animation_breathe_cur = BREATHE_MIN_STEP;
-                    breathe_dir = 1;
-                }
-            }
-            return false;
-        case L_T_PTD:
-            if (record->event.pressed) {
-                led_animation_direction = !led_animation_direction;
-            }
-            return false;
         case PC1:
             if (record->event.pressed) {
                 pc1_timer = timer_read();
@@ -219,75 +153,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return true; //Process all other keycodes normally
     }
 }
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-    switch (get_highest_layer(state)) {
-        case NIL:
-          led_lighting_mode = LED_MODE_NORMAL;
-          led_animation_breathing = false;
-          gcr_desired = LED_GCR_MAX - 80;
-          break;
-        case FN:
-          led_lighting_mode = LED_MODE_NORMAL;
-          led_animation_breathing = false;
-          gcr_desired = LED_GCR_MAX;
-          break;
-        case META:
-          led_lighting_mode = LED_MODE_NORMAL;
-          led_animation_breathing = false;
-          gcr_desired = 40;
-          break;
-        case OFF:
-          led_lighting_mode = LED_MODE_NON_KEYS_ONLY;
-          led_animation_breathing = true;
-          gcr_breathe = gcr_desired;
-          led_animation_breathe_cur = BREATHE_MIN_STEP;
-          led_animation_speed = 1.0f;
-          breathe_dir = 1;
-          break;
-    }
-    return state;
-};
-
-led_instruction_t led_instructions[] = {
-    //LEDs are normally inactive, no processing is performed on them
-    //Flags are used in matching criteria for an LED to be active and indicate how to color it
-    //Flags can be found in tmk_core/protocol/arm_atsam/led_matrix.h (prefixed with LED_FLAG_)
-    //LED IDs can be found in config_led.h in the keyboard's directory
-    //Examples are below
-
-    { .flags = LED_FLAG_USE_PATTERN, .pattern_id = 2 },
-
-    //All LEDs use the user's selected pattern (this is the factory default)
-    // { .flags = LED_FLAG_USE_ROTATE_PATTERN },
-
-    //Specific LEDs use the user's selected pattern while all others are off
-    // { .flags = LED_FLAG_MATCH_ID | LED_FLAG_USE_ROTATE_PATTERN, .id0 = 0xFFFFFFFF, .id1 = 0xAAAAAAAA, .id2 = 0x55555555, .id3 = 0x11111111 },
-
-    //Specific LEDs use specified RGB values while all others are off
-    // { .flags = LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .id0 = 0xFF, .id1 = 0x00FF, .id2 = 0x0000FF00, .id3 = 0xFF000000, .r = 75, .g = 150, .b = 225 },
-
-    //All LEDs use the user's selected pattern
-    //On layer 1, all key LEDs (except the top row which keeps active pattern) are red while all edge LEDs are green
-    //When layer 1 is active, key LEDs use red    (id0  32 -  16: 1111 1111 1111 1111 1000 0000 0000 0000 = 0xFFFF8000) (except top row 15 - 1)
-    //When layer 1 is active, key LEDs use red    (id1  64 -  33: 1111 1111 1111 1111 1111 1111 1111 1111 = 0xFFFFFFFF)
-    //When layer 1 is active, key LEDs use red    (id2  67 -  65: 0000 0000 0000 0000 0000 0000 0000 0111 = 0x00000007)
-    //When layer 1 is active, edge LEDs use green (id2  95 -  68: 1111 1111 1111 1111 1111 1111 1111 1000 = 0xFFFFFFF8)
-    //When layer 1 is active, edge LEDs use green (id3 105 -  96: 0000 0000 0000 0000 0000 0011 1111 1111 = 0x000003FF)
-    /* { .flags = LED_FLAG_USE_ROTATE_PATTERN }, */
-    /* { .flags = LED_FLAG_MATCH_ID | LED_FLAG_MATCH_LAYER | LED_FLAG_USE_RGB, .id0 = 0xFFFF8000, .id1 = 0xFFFFFFFF, .id2 = 0x00000007, .r = 255, .layer = 1 }, */
-    /* { .flags = LED_FLAG_MATCH_ID | LED_FLAG_MATCH_LAYER | LED_FLAG_USE_RGB, .id2 = 0xFFFFFFF8, .id3 = 0x000003FF, .g = 127, .layer = 1 }, */
-
-    //All key LEDs use red while edge LEDs use the active pattern
-    //All key LEDs use red     (id0  32 -   1: 1111 1111 1111 1111 1111 1111 1111 1111 = 0xFFFFFFFF)
-    //All key LEDs use red     (id1  64 -  33: 1111 1111 1111 1111 1111 1111 1111 1111 = 0xFFFFFFFF)
-    //All key LEDs use red     (id2  67 -  65: 0000 0000 0000 0000 0000 0000 0000 0111 = 0x00000007)
-    //Edge uses active pattern (id2  95 -  68: 1111 1111 1111 1111 1111 1111 1111 1000 = 0xFFFFFFF8)
-    //Edge uses active pattern (id3 105 -  96: 0000 0000 0000 0000 0000 0011 1111 1111 = 0x000003FF)
-    // { .flags = LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .id0 = 0xFFFFFFFF, .id1 = 0xFFFFFFFF, .id2 = 0x00000007, .r = 255 },
-    // { .flags = LED_FLAG_MATCH_ID | LED_FLAG_USE_ROTATE_PATTERN , .id2 = 0xFFFFFFF8, .id3 = 0x000003FF },
-
-    //end must be set to 1 to indicate end of instruction set
-     { .end = 1 }
-};
-
